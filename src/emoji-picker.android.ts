34a9d6a380dc1da40b764a32413fb34fc33a4d35
Hannes Rüger <hannesrueger@gmx.de>
2019-08-17 16:56:20 +0200

    initial commit
    added all the files from cloned seed repo

---
+++ src/emoji-picker.android.ts
@@ -0,0 +1,5 @@
+import { Common } from './emoji-picker.common';
+
+export class EmojiPicker extends Common {
+
+}
---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { Common } from './emoji-picker.common';
+export declare class EmojiPicker extends Common {
+  // define your typings manually
+  // or..
+  // take the ios or android .d.ts files and copy/paste them here
+}
